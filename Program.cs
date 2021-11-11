f5d7ed92f6326c26c0bed429ddb798fb4bff88ba
Steve Schaner <schanerst@gmail.com>
2021-11-10 21:20:42 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab9StudentDatabaseDeluxe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Lab 9: Student Database Deluxe";
+        }
+    }
+}
